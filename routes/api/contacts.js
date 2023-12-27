d2e7e2d5999ea7a65d6c19b3941f3e5d3af8c2b2
Chalaya22 <chalayaolga22@gmail.com>
2023-12-27 20:48:35 +0200

    comit 6

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const contacts = require("../../models/contacts");
 const Joi = require("joi");
 
-const router = express.Router(); // метод Router() создаал страницу web-serverа с маршрутами контактов
+const router = express.Router();
 const { HttpError } = require("../../helpers");
 
 const addSchema = Joi.object({
