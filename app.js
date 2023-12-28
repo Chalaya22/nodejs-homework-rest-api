4bc976c917869708d267710a333c00200df30c17
Chalaya22 <chalayaolga22@gmail.com>
2023-12-28 20:29:51 +0200

    comit 13

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, massage = "Server error" } = err;
-  res.status(err.status || 500).json({ massage });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
