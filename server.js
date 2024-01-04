7129ee84aaa87293d7e86d6c6b8d061c01ce12c0
Chalaya22 <chalayaolga22@gmail.com>
2024-01-04 16:29:27 +0200

    comit  9

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 mongoose.set("strictQuery", true);
-// const { DB_HOST } = process.env;
-const DB_HOST =
-  "mongodb+srv://Chala22:NKuZNa7vcyB1QMed@cluster0.4nnooff.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
